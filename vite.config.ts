645cdb270ce9f787c5ceb23ca2800ccddb759c38
ostrov9878 <ostroverkhov2011@gmail.com>
2026-05-06 16:28:09 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Specifically enable Buffer and global for @circle-fin/app-kit
+      include: ['buffer', 'process', 'util'],
+    }),
+  ],
+  define: {
+    global: 'globalThis',
+  },
+})
